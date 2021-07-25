dd23f32570891accd8eb8161ce9047e077ad3d26
Sheng Chuan (Michael) Hsu <cool96482@hotmail.com>
2021-07-25 19:00:18 +0800

    check is prettier working

--- server/index.js
+++ server/index.js
@@ -12,45 +12,53 @@ const run = async () => {
       // Get JIRA issues with JQL
       const jiraData = await searchForIssuesUsingJQL(user.jiraEmail, user.jiraToken);
       if (!jiraData.issues || !Array.isArray(jiraData.issues)) {
-        console.log('No ticket need to update in JIRA.')
-        return
+        console.log('No ticket need to update in JIRA.');
+        return;
       }
 
       // Get Notion issues
       const notion = new Client({ auth: user.notionToken });
-      const notionData = await queryIssues(notion, user.notionDatabaseId)
+      const notionData = await queryIssues(notion, user.notionDatabaseId);
       if (!notionData.results || !Array.isArray(notionData.results)) {
-        console.log('The Notion result is empty.')
-        return
+        console.log('The Notion result is empty.');
+        return;
       }
 
-      let needAddItems = []
-      let needUpdateItems = []
+      let needAddItems = [];
+      let needUpdateItems = [];
 
       for (const issue of jiraData.issues) {
-        let isExist = false
+        let isExist = false;
         notionData.results.forEach(notionIssue => {
           // Check whether the current JIRA issue already exists in Notion
           if (notionIssue.properties['JIRA Key']?.rich_text[0]?.text?.content === issue.key) {
-            isExist = true
+            isExist = true;
 
             // Check whether the current status of JIRA issue already equals in Notion
             if (notionIssue.properties['Status']?.select?.name !== issue.fields.status.name) {
               // If the status is not equal, then push the issue into needUpdateItems array
-              needUpdateItems.push({ notionPageId: notionIssue.id, ...issue})
+              needUpdateItems.push({ notionPageId: notionIssue.id, ...issue });
             }
           }
-        })
+        });
 
         if (!isExist) {
           // If the issue is not exist in Notion, then push the issue into needAddItems array
-          needAddItems.push(issue)
+          needAddItems.push(issue);
         }
       }
 
       // Start add new issue into Notion
       for (const issue of needAddItems) {
-        addIssue(notion, user.notionDatabaseId, issue.fields.status.name === 'Done', issue.fields.status.name ,issue.fields.summary, issue.key, `https://17media.atlassian.net/browse/${issue.key}`);
+        addIssue(
+          notion,
+          user.notionDatabaseId,
+          issue.fields.status.name === 'Done',
+          issue.fields.status.name,
+          issue.fields.summary,
+          issue.key,
+          `https://17media.atlassian.net/browse/${issue.key}`,
+        );
       }
 
       // Start update new issue into Notion
